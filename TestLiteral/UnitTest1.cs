fbad45b196d8685e55a15c69811efbf52ce05565
Eduardo Sossa <Eduardo Sossa@DESKTOP-812FA3V>
2021-02-05 21:51:03 -0400

    unitest realizados

--- TestLiteral/UnitTest1.cs
+++ TestLiteral/UnitTest1.cs
@@ -42,5 +42,38 @@ namespace UNITest
 
             Assert.AreEqual(esperado, resultado);
         }
+        [Test]
+        public void TestNumeroGet2()
+        {
+            //arrange preparacion
+            string num1 = "5";
+            string esperado = "Cinco";
+
+            LiteralController lit = new LiteralController();
+
+            //act= ejecucion
+
+            string resultado = lit.NumeroGet(num1);
+
+            //assert=verificacion
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        public void TestNumeroPost2()
+        {
+            //arrange preparacion
+            string num1 = "5";
+            string esperado = "Cinco";
+
+            LiteralController lit = new LiteralController();
+
+            //act= ejecucion
+
+            string resultado = lit.NumeroPost(num1);
+
+            //assert=verificacion
+
+            Assert.AreEqual(esperado, resultado);
+        }
     }
 }
